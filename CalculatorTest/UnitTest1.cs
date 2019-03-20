f3c33827876dbf85cb5e6bea16ca9f7cceb0f00c
maryamshahidi-git <maryshahidi93@gmail.com>
2019-03-20 16:23:25 -0400

    second commit

---
+++ CalculatorTest/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using CalculatorLogic;
+//using Xunit;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculatorTest
+{
+    public class UnitTest1
+    {
+        [TestClass]
+        public void ShouldAddTwoNumbers()
+        {
+            var x = new Calculator().Sum(1, 2);
+            Assert.AreEqual(x, 3);
+        }
+
+        [TestClass]
+        public void ShouldMultiplyTwoNumbers()
+        {
+            var x = new Calculator().Multiply(3, 2);
+            Assert.AreEqual(6, x);
+        }
+    }
+}
